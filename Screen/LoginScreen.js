ace2e121ba5d7c1e0f1f36429ec9a190faa81450
Sabin Regmi <get2sabin@gmail.com>
2023-04-11 20:30:26 -0400

    Fixed LoginScreen

--- Screen/LoginScreen.js
+++ Screen/LoginScreen.js
@@ -17,10 +17,14 @@ export default function LoginScreen({ route, navigation }) {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-slate-50">
-      <View className="p-8 w-full max-w-sm">
-        <Text className="text-5xl font-bold mb-6 text-slate-900">Login</Text>
+    <View className="flex-1 items-center pt-12 justify-start bg-slate-50">
+      <View className="bg-white h-12 mt-12 mb-12 shadow-md w-60 items-center justify-center rounded-md">
+        <Text className="text-1xl font-bold text-gray-500">The Q </Text>
+      </View>
+      <View className="p-8 mt-12 w-full max-w-sm">
+        <Text className="text-2xl font-bold mb-6 text-slate-900">Login to</Text>
 
+        <Text className="mb-1">Username</Text>
         <TextInput
           className="w-full bg-white border border-slate-200 rounded-md h-12 px-4 mb-4"
           placeholderTextColor="#000"
@@ -28,7 +32,7 @@ export default function LoginScreen({ route, navigation }) {
           value={email}
           onChangeText={(text) => setEmail(text)}
         />
-
+        <Text className="mb-1">Password</Text>
         <TextInput
           className="w-full bg-white border border-slate-200 rounded-md h-12 px-4"
           placeholderTextColor="#000"
@@ -37,16 +41,27 @@ export default function LoginScreen({ route, navigation }) {
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
-
+        <Pressable className="mt-1 mb-5 ">
+          <View>
+            <Text className="justify-end text-right">Forgot Password?</Text>
+          </View>
+        </Pressable>
         <Pressable
-          className="h-12 bg-purple-500 rounded-md flex flex-row justify-center items-center px-6 mt-4"
+          className="h-12 w-40 bg-black rounded-md mx-auto flex flex-row justify-center items-center px-6 mt-4"
           onPress={handleLogin}
         >
           <View className="flex-1 flex items-center">
-            <Text className="text-white text-base font-medium">Login</Text>
+            <Text className="text-white text-base font-medium">Sign In</Text>
           </View>
         </Pressable>
       </View>
+      <Pressable className="mt-1 mb-5 ">
+        <View>
+          <Text className="justify-end text-right">
+            Not a member yet? Join Now
+          </Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
